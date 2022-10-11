002aaabc9add85c9faa647d6050efc283c9bfb18
ele-dar <104769631+ele-dar@users.noreply.github.com>
2022-10-11 11:24:09 +0300

    working calculator

---
+++ index.js
@@ -0,0 +1,68 @@
+const screen = document.querySelector('.screen')
+const keys = Array.from(document.querySelectorAll('button'))
+
+let display = 0
+let memory = 0
+let operatorMemory = ''
+
+keys.forEach(key => {
+    key.addEventListener('click', (e) => handleBtnClick(e.target))
+})
+
+const handleBtnClick = (btn) => {
+    switch (btn.id) {
+        case '':
+            display = concatenate(display, btn.innerHTML)
+            break
+        case 'point':
+            if (display.toString().includes('.')) break
+            display = display.toString().concat(btn.innerHTML)
+            break
+        case 'delete':
+            display = removeLastElement(display)
+            break
+
+        case 'add':
+        case 'subtract':
+        case 'divide':
+        case 'multiply':
+            memory = operatorMemory ? calculate[operatorMemory](+memory, +display) : display
+            operatorMemory = btn.id
+            display = 0
+            break
+
+        case 'equal':
+            if (operatorMemory) {
+                memory = calculate[operatorMemory](+memory, +display)
+                display = 0
+            }
+            break
+        case 'reset':
+            display = 0
+            memory = 0
+            operatorMemory = 0
+            break
+
+    }
+    screen.innerHTML = display ? display : memory
+}
+
+const concatenate = (value1, value2) => {
+    if (value1 == 0 && !value1.toString().includes('.')) return value2
+    return value1 + value2
+}
+
+const removeLastElement = (value) => {
+    if (value.toString().length === 1) return 0
+    return value.toString().slice(0, -1)
+}
+
+const calculate = {
+    'add': (x, y) => x + y,
+    'subtract': (x, y) => x - y,
+    'divide': (x, y) => x / y,
+    'multiply': (x, y) => x * y
+}
+
+
+
