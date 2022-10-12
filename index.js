fefb2ffcb9378622831e796c6aa57ca7bb269b4e
ele-dar <104769631+ele-dar@users.noreply.github.com>
2022-10-12 13:05:59 +0300

    finished app

--- index.js
+++ index.js
@@ -5,48 +5,6 @@ let display = 0
 let memory = 0
 let operatorMemory = ''
 
-keys.forEach(key => {
-    key.addEventListener('click', (e) => handleBtnClick(e.target))
-})
-
-const handleBtnClick = (btn) => {
-    switch (btn.id) {
-        case '':
-            display = concatenate(display, btn.innerHTML)
-            break
-        case 'point':
-            if (display.toString().includes('.')) break
-            display = display.toString().concat(btn.innerHTML)
-            break
-        case 'delete':
-            display = removeLastElement(display)
-            break
-
-        case 'add':
-        case 'subtract':
-        case 'divide':
-        case 'multiply':
-            memory = operatorMemory ? calculate[operatorMemory](+memory, +display) : display
-            operatorMemory = btn.id
-            display = 0
-            break
-
-        case 'equal':
-            if (operatorMemory) {
-                memory = calculate[operatorMemory](+memory, +display)
-                display = 0
-            }
-            break
-        case 'reset':
-            display = 0
-            memory = 0
-            operatorMemory = 0
-            break
-
-    }
-    screen.innerHTML = display ? display : memory
-}
-
 const concatenate = (value1, value2) => {
     if (value1 == 0 && !value1.toString().includes('.')) return value2
     return value1 + value2
@@ -64,5 +22,41 @@ const calculate = {
     'multiply': (x, y) => x * y
 }
 
+keys.forEach(key => {
+    key.addEventListener('click', (e) => handleBtnClick(e.target))
+})
 
-
+const handleBtnClick = (btn) => {
+    switch (btn.id) {
+        case '':
+            display = concatenate(display, btn.innerHTML)
+            break
+        case 'point':
+            if (display.toString().includes('.')) break
+            display = display.toString().concat(btn.innerHTML)
+            break
+        case 'delete':
+            display = removeLastElement(display)
+            break
+        case 'add':
+        case 'subtract':
+        case 'divide':
+        case 'multiply':
+            memory = operatorMemory ? calculate[operatorMemory](+memory, +display) : display
+            operatorMemory = btn.id
+            display = 0
+            break
+        case 'equal':
+            if (operatorMemory) {
+                memory = calculate[operatorMemory](+memory, +display)
+                display = 0
+            }
+            break
+        case 'reset':
+            display = 0
+            memory = 0
+            operatorMemory = 0
+            break
+    }
+    screen.innerHTML = display ? display : memory
+}
--- themes/theme.js
+++ themes/theme.js
@@ -1,13 +1,30 @@
-const themeSwitch = document.querySelector('.theme-switch')
-const themeSelectors = document.querySelectorAll('input[name=theme]')
+const themeSelectors = Array.from(document.querySelectorAll('input[name=theme]'))
 const themeStylesheet = document.getElementById('theme-stylesheet')
 
-console.log(themeStylesheet)
-themeSelectors.forEach(radio => radio.addEventListener('change', e => handleThemeChange(e.target)))
-
-const handleThemeChange = (target) => {
-    console.log(target)
-    themeStylesheet.setAttribute('href', `themes/${target.id}.css`)
+const setColorScheme = (scheme) => {
+    themeStylesheet.setAttribute('href', `themes/${scheme}.css`)
+    themeSelectors.find(selector => selector.id === scheme).checked = true
 }
 
-// const initialTheme
+const getPreferredColorScheme = () => {
+    if (sessionStorage.getItem('preferred-color-scheme')) return sessionStorage.getItem('preferred-color-scheme')
+    if (window.matchMedia) {
+        if (window.matchMedia('(prefers-color-scheme: light)').matches) return 'light'
+    }
+    return 'dark'
+}
+
+setColorScheme(getPreferredColorScheme())
+
+if (window.matchMedia) {
+    const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: light)')
+    colorSchemeQuery.addEventListener('change', () => {
+        setColorScheme(getPreferredColorScheme())
+    })
+}
+
+themeSelectors.forEach(radio => radio.addEventListener('change', e => {
+    setColorScheme(e.target.id)
+    sessionStorage.setItem('preferred-color-scheme', e.target.id)
+})
+)
